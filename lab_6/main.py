e2e9763d0c54090d280ea8309e15e966ba06331c
ZagrebelnyiVA <vlad.z.00@inbox.ru>
2024-12-11 12:40:20 +0600

    lab_6 final done

--- lab_6/main.py
+++ lab_6/main.py
@@ -5,6 +5,7 @@ from function.factorize import *
 def clear():
     system("cls||clear")
 
+
 clear()
 while True:
     print("1.Факторизовать число методом квадратичного решета.")
@@ -15,23 +16,22 @@ while True:
             clear()
             m = int(input("Введите m: "))
             while True:
-                abc = [
+                a, b, c = [
                     int(x) for x in input("Введите a, b, c через пробел: ").split(" ")
                 ]
-                print(abc)
                 if (
-                    gcd_ext(abc[0], abc[1])[0] != 1
-                    or gcd_ext(abc[0], abc[2])[0] != 1
-                    or gcd_ext(abc[1], abc[2])[0] != 1
+                    gcd_ext(a, b)[0] != 1
+                    or gcd_ext(a, c)[0] != 1
+                    or gcd_ext(b, c)[0] != 1
                 ):
                     print("Введены некорректные значения")
                     continue
                 break
-            mults = factorize_sieve(m, abc[0], abc[1], abc[2])
+            mults = factorize_sieve(m, a, b, c)
             if mults == None:
                 print("Данное m простое")
             else:
-                print(f"{m} = {mults[0]} * {mults[1]}")
+                print(f"Ответ: {m} = {mults[0]} * {mults[1]}")
             input()
             clear()
         case "2":
@@ -40,12 +40,12 @@ while True:
             x0_1, x0_2 = [
                 int(x) for x in input("Введите x0_1 и x0_2 через пробел: ").split(" ")
             ]
-            try:
-                mults = factorize_ro(m, x0_1, x0_2)
+            mults = factorize_ro(m, x0_1, x0_2)
+            if mults != None:
                 print(
-                    f"На {mults[1]} шаге алгоритма получено значение d{mults[1]}={mults[0][0]}\nОтвет: {m}={mults[0][0]}*{mults[0][1]}"
+                    f"На {mults[1]} шаге алгоритма получено значение d{mults[1]}={mults[0][0]}\nОтвет: {m} = {mults[0][0]} * {mults[0][1]}"
                 )
-            except:
+            else:
                 print("Данное m простое")
             input()
             clear()
